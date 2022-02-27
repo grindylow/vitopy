e4bdfd5768ea726c88a1477c41c4caf7c432a9ab
Martin Grill <martin.grill@gmx.de>
2022-02-27 08:34:38 +0100

    various modifications that got lost due to corrupted repo

--- vitopy.py
+++ vitopy.py
@@ -10,7 +10,7 @@ from vitolib import optolink
 from vitolib.optomessage import *
 import logging
 import signal
-from vcd import VCDWriter
+#from vcd import VCDWriter
 import paho.mqtt.client as mqtt   # pip3 install paho-mqtt
 
 PROG = 'vitopy'
@@ -84,6 +84,27 @@ def parse_config_file(filename):
     else:
         cfg = json.load(open(filename,'r',encoding='cp437'))
     logging.info(cfg)
+
+    # some post-processing so we can use our configuration
+    # data more easily
+
+    # - add unique id to each actual data point
+    # - convert all addresses to integer
+
+    uid = 1
+    for v in cfg["datapoints"]:
+        logging.debug(v)
+        if not 'addr' in v:
+            continue
+        v['uid'] = uid
+        uid = uid+1
+        if type(v['addr']) is str:
+            v['addr'] = int(v['addr'],0)  # parse non-decimal numbers
+        if not 'id' in v:
+            v['id'] = 'NOID(%s)'%v['addr']
+        if not 'TTL' in v:
+            v['TTL'] = 900
+    
     return cfg
 
 def setup_logging():
@@ -105,10 +126,6 @@ def log_to_vcd(cfg):
     for v in config['datapoints']:
         if not 'addr' in v:
             continue
-        if not 'id' in v:
-            v['id'] = 'NOID'
-        if type(v['addr']) is str:
-            v['addr'] = int(v['addr'], 0)  # parse non-decimal numbers
         name = '%s_%s'%(v['id'],v['addr'])
         if 'uint8'==v['type']:
             logging.info('Adding variable %s' % v['addr'])
@@ -117,6 +134,9 @@ def log_to_vcd(cfg):
             logging.info('Adding variable %s' % v['addr'])
             vcdvars.update( { v['addr']: vcdwriter.register_var('vitopy',name,'integer',size=8) } )
         if 'uint16'==v['type']:
+            logging.info('Adding variable %s' % v['addr'])
+            vcdvars.update( { v['addr']: vcdwriter.register_var('vitopy',name,'integer',size=16) } )
+        if 'int16'==v['type']:
             logging.info('Adding variable %s' % v['addr'])
             vcdvars.update( { v['addr']: vcdwriter.register_var('vitopy',name,'integer',size=16) } )
         if 'uint32'==v['type']:
@@ -137,8 +157,16 @@ def log_to_vcd(cfg):
             r = allreadings[k]
             if k in vcdvars:
                 vcdwriter.change(vcdvars[k], now_utc.timestamp(), r)
+                # k is the address, look up the ID-string associated with this address
+                id = ''
+                for point in config['datapoints']:
+                    if not 'addr' in point:
+                        continue
+                    if point['addr'] == k:
+                        id = point['id']
+                        break
                 if args.publishmqtt:
-                    mqttc.publish(str(k), r)
+                    mqttc.publish(id, r)
             
 
         vcdwriter.flush()
@@ -155,6 +183,98 @@ def log_to_vcd(cfg):
     o.close_port()
     vcdwriter.close()
     
+    return
+
+
+def cyclically_poll_heating(cfg):
+    """
+    Cyclically poll heating. Send any values that have changed to various
+    outputs as configured by the command line arguments.
+    """
+    global SIGINT
+
+    # We read all values for which our configuration has a
+    # unique ID.
+
+    # @future: where would be best implement aggregated blocks of readings?
+
+    # We remember the previous reading, so we can react to changes
+    prev_vals = {}
+    
+    # poll heating
+    o = optolink.OptoLink()
+    o.open_port(args.port)
+    o.init()
+    
+    while not SIGINT:
+        logging.info('-- start of query --')
+        
+        for v in cfg['datapoints']:
+            if not 'uid' in v:
+                continue
+        
+            logging.debug(v)
+            now = time.time()
+            m = ReadRequest(v['addr'], v['bytes'])
+
+            reply = o.query(m.assemble())
+            if isinstance(reply, ErrorReply):
+                logging.warning('request for addr %s resulted in ErrorReply.'%hex(v['addr']))
+                continue
+            
+            r = {}
+            t = v['type']
+            if 'uint8'==t:
+                r = reply.get_readings_as_uint8()
+            elif 'uint16'==t:
+                r = reply.get_readings_as_uint16()
+            elif 'int16'==t:
+                r = reply.get_readings_as_int16()
+            elif 'uint32'==t:
+                r = reply.get_readings_as_uint32()
+
+            # r is an array of (addr, val) tuples
+            # But currently we only ever retrieve one reading
+            assert(len(r)==1)
+            assert(r[0][0]==v['addr'])
+            val = r[0][1]
+
+            # only output if changed or previous value too old
+            remain_quiet = True
+            if v['uid'] not in prev_vals:
+                logging.info('NOT remaining quiet about this value because we don''t have a previous value yet: %s=%s',
+                             v['id'], val)
+                prev_vals[v['uid']] = {'last_changed':0, 'val':None}
+                remain_quiet = False
+            elif prev_vals[v['uid']]['last_changed']+v['TTL'] < now:
+                logging.info('NOT remaining quiet about this value because time-to-live has expired: %s=%s',
+                             v['id'], val)
+                remain_quiet = False
+            elif prev_vals[v['uid']]['val'] != val:
+                logging.info('NOT remaining quiet about this value because value has changed: %s changed from %s to %s',
+                             v['id'], prev_vals[v['uid']]['val'], val)
+                remain_quiet = False
+            # @future: could also implement hysteresis
+
+            if not remain_quiet:
+                prev_vals[v['uid']]['last_changed'] = now
+                prev_vals[v['uid']]['val'] = val
+                if args.publishmqtt:
+                    mqttc.publish(v['id'], val)
+            else:
+                logging.info('remaining quiet about this value: %s', val)
+
+        # sleep, unless rudely interrupted
+        for i in range(10):
+            time.sleep(1)
+            if SIGINT:
+                logging.info('-- interrupted by SIGINT --')
+                break
+        
+        logging.info('-- end of query --')
+        
+    o.deinit()
+    o.close_port()
     return
     
 if __name__ == "__main__":
@@ -174,7 +294,15 @@ if __name__ == "__main__":
         mqttc.enable_logger()
         mqttc.connect("localhost")
         mqttc.loop_start()
-    
+
+    cyclically_poll_heating(config)
+        
+    if args.publishmqtt:
+        mqttc.disconnect()
+
+    sys.exit(0)
+
+    # old stuff
     if args.vcd:
         log_to_vcd(config)
         if args.publishmqtt:
